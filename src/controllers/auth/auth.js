441fa2a721a260c47d5de6faa9199f230ed7818a
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-12-01 21:47:10 +0500

    add: refreshToken controller created

--- src/controllers/auth/auth.js
+++ src/controllers/auth/auth.js
@@ -49,30 +49,66 @@ export const loginCustomer = async (req, res) => {
 }
 
 export const loginDeliveryPartner = async (req, res) => {
-    try {
-      const { email,password } = req.body
-  
-      let deliveryPartner = await DeliveryPartner.findOne({ email })
-  
-      if (!deliveryPartner) {
-        // send error!
-        return res.status(404).json({ message: 'Delivery Partner not found' })
-       
-      }
+  try {
+    const { email, password } = req.body
 
-      const isMatch = deliveryPartner.password === password
+    let deliveryPartner = await DeliveryPartner.findOne({ email })
 
-      if(!isMatch){
-        return res.status(404).json({ message: 'Password is incorrect' })
-      }
-
-  
-      const { accessToken, refreshToken } = generateToken(deliveryPartner)
-  
-      res
-        .status(200)
-        .json({ accessToken, refreshToken, deliveryPartner,message: ' Login Successfully' })
-    } catch (error) {
-      res.status(500).json({ message: error.message })
+    if (!deliveryPartner) {
+      // send error!
+      return res.status(404).json({ message: 'Delivery Partner not found' })
     }
+
+    const isMatch = deliveryPartner.password === password
+
+    if (!isMatch) {
+      return res.status(404).json({ message: 'Password is incorrect' })
+    }
+
+    const { accessToken, refreshToken } = generateToken(deliveryPartner)
+
+    res
+      .status(200)
+      .json({
+        accessToken,
+        refreshToken,
+        deliveryPartner,
+        message: ' Login Successfully'
+      })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
   }
+}
+
+export const refreshToken = async (req, res) => {
+  try {
+    const { refreshToken } = req.body
+
+    // if no refresh token, then return an error
+    if (!refreshToken) {
+      return res.status(401).json({ message: 'Refresh token required' })
+    }
+
+    // if refresh token is valid for user
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
+
+    let user
+
+    if (decoded.role === 'Customer') {
+      user = await Customer.findById(decoded.userId)
+    } else {
+      user = await DeliveryPartner.findById(decoded.userId)
+    }
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+
+    const { accessToken, refreshToken: newRefreshToken } = generateToken(user)
+
+    // RETURN RESPONSE!
+    res.status(200).json({ accessToken, refreshToken: newRefreshToken })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+}
