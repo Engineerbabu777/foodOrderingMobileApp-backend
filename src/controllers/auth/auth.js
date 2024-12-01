0da631305039d873cdd3e33d8525c2e7dcbb9d63
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-12-01 21:42:33 +0500

    add: login delivery partner controller created

--- src/controllers/auth/auth.js
+++ src/controllers/auth/auth.js
@@ -1,11 +1,11 @@
 import jwt from 'jsonwebtoken'
-import { Customer } from '../../models/user.model.js'
+import { Customer, DeliveryPartner } from '../../models/user.model.js'
 
 const generateToken = user => {
   const accessToken = jwt.sign(
     { userId: user._id, role: user.role },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: '30s' }
+    { expiresIn: '1h' }
   )
 
   const refreshToken = jwt.sign(
@@ -47,3 +47,32 @@ export const loginCustomer = async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 }
+
+export const loginDeliveryPartner = async (req, res) => {
+    try {
+      const { email,password } = req.body
+  
+      let deliveryPartner = await DeliveryPartner.findOne({ email })
+  
+      if (!deliveryPartner) {
+        // send error!
+        return res.status(404).json({ message: 'Delivery Partner not found' })
+       
+      }
+
+      const isMatch = deliveryPartner.password === password
+
+      if(!isMatch){
+        return res.status(404).json({ message: 'Password is incorrect' })
+      }
+
+  
+      const { accessToken, refreshToken } = generateToken(deliveryPartner)
+  
+      res
+        .status(200)
+        .json({ accessToken, refreshToken, deliveryPartner,message: ' Login Successfully' })
+    } catch (error) {
+      res.status(500).json({ message: error.message })
+    }
+  }
